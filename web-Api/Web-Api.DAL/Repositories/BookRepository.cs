8fbace258c65cf5203b80e8c8967eff797047b6b
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-02-16 22:39:40 +0300

    Update libraries version

--- web-Api/Web-Api.DAL/Repositories/BookRepository.cs
+++ web-Api/Web-Api.DAL/Repositories/BookRepository.cs
@@ -14,7 +14,7 @@ namespace Web_Api.DAL.Repositories
 
         public async Task CreateBookAsync(Book book, CancellationToken cancellationToken = default) => await CreateAsync(book, cancellationToken);
         public IQueryable<Book> FindBookById(int id) => FindByCondition(b => b.Id == id).Include(b => b.Author);
-        public IQueryable<Book> GetAllBooks() => GetAll().Include(b => b.Author);
+        public IQueryable<Book> GetAllBooks() => GetAll().Include(b => b.Author.FirstName);
         public async Task UpdateBookAsync(Book book, CancellationToken cancellationToken = default) => await UpdateAsync(book, b => b.Id == book.Id, cancellationToken);
         public async Task DeleteBookAsync(int id, CancellationToken cancellationToken = default) => await DeleteAsync(b => b.Id == id, cancellationToken);
     }
